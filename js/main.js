8bc2cab6ffd765b997e740bc96ff7edb9751da1a
sol di-falco <msoldifalco86@gmail.com>
2023-05-26 13:18:45 -0300

    agregué carpeta y 1 archivo js donde realicé una función que me permite que al tocar una imagen me aparzcan unas cards

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//funcion que me permite mostrar y ocultar contenido de unas cards tocando una imagen con 
+//los gatos recén ingresados:
+
+//funcion mostrar:
+
+function mostrar (){
+    document.getElementById('img-gato-mostrar').style.display = 'block';
+}
+
+//funcion ocultar:
+
+function ocultar (){
+    document.getElementById('img-gato-mostrar').style.display = 'none';
+}
