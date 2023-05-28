ad2c38d72aa6eb204f36e09dbfe09741e6707f8b
sol di-falco <msoldifalco86@gmail.com>
2023-05-28 14:14:18 -0300

    agregué archivo js para la pagina adopción y realicé una función que me permité cuando toco un botón ver una card con el contenido de los items para adoptar de forma responsable

---
+++ js/adopcion.js
@@ -0,0 +1,11 @@
+//realizaré para la página adopción una función que me permita ver los items para una adopción responsable tocando un botón 
+
+function mostrarInfo (){
+    document.getElementById('cerrar-info').style.display = 'block';
+}
+
+
+
+function cerrarInfo (){
+    document.getElementById('cerrar-info').style.display = 'none';
+}
